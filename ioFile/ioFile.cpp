f1cdbd2f065c6e06e43b4a4cc159cecd7347592a
raditanurhasna <raditanurhasna1410@gmail.com>
2023-06-21 14:43:55 +0700

    Add project files.

---
+++ ioFile/ioFile.cpp
@@ -0,0 +1,16 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	string baris;
+
+	//membuka file dalam mode menulis.
+	ofstream outfile;
+	//menunjuj ke sebuah nama file
+	outfile.open("contohfile.txt");
+
+	cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+
+}
