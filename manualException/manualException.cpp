458b93c1b3e2c77c4280bc98b66462a8f323927a
raditanurhasna <raditanurhasna1410@gmail.com>
2023-06-21 15:58:59 +0700

    Digunakan untuk menutup file

--- manualException/manualException.cpp
+++ manualException/manualException.cpp
@@ -8,11 +8,11 @@ int main()
 		throw 0.5; //melemparkan sebuah integer maka
 		cout << "Pernyataan tidak akan dieksekusi" << endl;
 	}
-	cath  (int a) {
+	catch  (int a) {
 		//blok ini akan dieksekusi
 		cout << "Pengecualian akan dieksekusi" << endl;
 	}
-	casth(...) {
+	catch(...) {
 		/*jika selain integer maka block ini akan dieksekusi*/
 		cout << "default Pengecualian Dieksekusi " << endl;
 	}
