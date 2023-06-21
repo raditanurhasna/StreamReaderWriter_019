cfcf6f858ec49538d101164c7f1f730a8af092b6
raditanurhasna <raditanurhasna1410@gmail.com>
2023-06-21 15:43:43 +0700

    Digunakan untuk blok yang dieksekusi

--- manualException/manualException.cpp
+++ manualException/manualException.cpp
@@ -8,8 +8,13 @@ int main()
 		throw 0.5; //melemparkan sebuah integer maka
 		cout << "Pernyataan tidak akan dieksekusi" << endl;
 	}
-	cath (int a) {
+	cath  (int a) {
 		//blok ini akan dieksekusi
 		cout << "Pengecualian akan dieksekusi" << endl;
 	}
+	casth(...) {
+		/*jika selain integer maka block ini akan dieksekusi*/
+		cout << "default Pengecualian Dieksekusi " << endl;
+	}
+	return 0;
 }
